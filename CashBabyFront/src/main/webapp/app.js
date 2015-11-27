4d6734595e6a80c350ceb39654c6950bdfbde6fe
maximepvrt <maximepvrt@gmail.com>
2015-11-27 17:50:38 +0100

    fix front

--- CashBabyFront/src/main/webapp/app.js
+++ CashBabyFront/src/main/webapp/app.js
@@ -1,12 +1,5 @@
 var app = angular.module('cashbaby', ['ui.router', 'ngStorage']);
 
-app.run(['$localStorage',
-    function($localStorage) {
-      if(!$localStorage.panier) {
-        $localStorage.panier = [];
-      }
-    }]);
-
 app.factory('CashbabyAPI', ['$q',
   function($q){
     return {
@@ -16,7 +9,7 @@ app.factory('CashbabyAPI', ['$q',
                data = typeof data !== 'undefined' ? data : {};
 $.soap({
   url: 'http://localhost:9763/services/Shop/',
-  namespaceURL:'http://shop.cashbaby.services.alma.org',
+  namespaceURL:'http://services.api.shop.cashbaby.services.alma.org',
   method: apiName,
   data: data,
   soap12: true,
@@ -36,12 +29,14 @@ return deferred.promise;
 app.run(['$localStorage', 'CashbabyAPI',
         function($localStorage, CashbabyAPI) {
 
+            if(!$localStorage.orderId) {
             $localStorage.orderId = null;
             CashbabyAPI.run('startOrder').then(function(data) {
               $localStorage.orderId = data.return;
             }, function(data) {
                console.log("ko");
             });
+          }
 
 
         }]);
@@ -140,25 +135,67 @@ app.controller('cashbaby.controller.produit', ['$scope', '$localStorage', '$stat
   }
 ]);
 
-app.controller('cashbaby.controller.panier', ['$scope', '$localStorage',
-  function clientList($scope, $localStorage) {
-    CashbabyAPI.run('getItem', {id: $stateParams.id}).then(function(data) {
-      $scope.produits = data.return;
+app.controller('cashbaby.controller.panier', ['$scope', '$localStorage', 'CashbabyAPI',
+  function clientList($scope, $localStorage, CashbabyAPI) {
+    CashbabyAPI.run('getCart', {orderId: $localStorage.orderId}).then(function(data) {
+      $scope.cart = data.return;
       console.log(data.return);
     });
     $scope.getTotal = function(){
-        var total = 0;
-        for(var i = 0; i < $scope.cart.products.length; i++){
-            var product = $scope.cart.products[i];
-            total += (product.price * product.quantity);
-        }
-        return total;
+        var values = 0; // declarate all vars, it's a good pratice
+        angular.forEach($scope.cart, function(produit, index) {
+          values += produit.quantity * produit.price;
+        });
+        return values;
+    }
+    $scope.removeOne = function(id) {
+      CashbabyAPI.run('unreserve', {orderId: $localStorage.orderId, itemId: id, quantity: 1}).then(function(data) {
+        console.log(data.return);
+        angular.forEach($scope.cart, function(produit, index) {
+          if(produit.id == id) {
+            $scope.cart[index].quantity -= 1;
+          }
+        });
+      });
+    }
+    $scope.addOne = function(id) {
+      CashbabyAPI.run('reserve', {orderId: $localStorage.orderId, itemId: id, quantity: 1}).then(function(data) {
+        console.log(data.return);
+        angular.forEach($scope.cart, function(produit, index) {
+          if(produit.id == id) {
+            $scope.cart[index].quantity = parseInt($scope.cart[index].quantity)+1;
+          }
+        });
+      });
+    }
+    $scope.removeAll = function(id, quantity) {
+      CashbabyAPI.run('unreserve', {orderId: $localStorage.orderId, itemId: id, quantity: quantity}).then(function(data) {
+        console.log(data.return);
+        angular.forEach($scope.cart, function(produit, index) {
+          if(produit.id == id) {
+            $scope.cart.splice(index, 1);
+          }
+        });
+      });
     }
   }
 ]);
 
-app.controller('cashbaby.controller.info', ['$scope', '$localStorage',
-  function clientList($scope, $localStorage) {
-
+app.controller('cashbaby.controller.info', ['$scope', '$localStorage', '$state', 'CashbabyAPI',
+  function clientList($scope, $localStorage, $state, CashbabyAPI) {
+    $scope.validCommand = function() {
+      $scope.card.expirationDate = $scope.month+'/'+$scope.year;
+      $scope.addressInline = $scope.card.ownerName+'<br>'+$scope.address.address+'<br>'+$scope.address.zipcode+'<br>'+$scope.address.city+'<br>'+$scope.address.country
+    CashbabyAPI.run('order', {orderId: $localStorage.orderId, shippingAddress: $scope.addressInline, card: $scope.card}).then(function(data) {
+      CashbabyAPI.run('startOrder').then(function(data) {
+              $localStorage.orderId = data.return;
+              $state.go('webapp.home')
+            }, function(data) {
+               console.log("ko");
+            });
+    }, function() {
+      console.log('ko');
+    });
+  }
   }
 ]);
